cc0cedff4697f0ae2c34d0102df534008cbc25c8
Ubuntu <ubuntu@ip-172-31-19-34.ec2.internal>
2026-03-27 12:09:35 +0000

    test trigger

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on the  port ${PORT}`);
 });
