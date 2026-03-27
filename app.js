dc8f137e217a4f3a7bcb28c7ecb5aec09a170a8e
Ubuntu <ubuntu@ip-172-31-19-34.ec2.internal>
2026-03-27 18:05:25 +0000

    testing pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from CI/CD Pipeline code 🚀");
+  res.send("Hello from CI/CD Pipeline code deploy succesful 🚀");
 });
 
 const PORT = 3000;
